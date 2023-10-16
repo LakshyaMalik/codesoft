239f615baec3e760d6c8a23612c1ca657168e27d
Lakshya Malik <103602572+LakshyaMalik@users.noreply.github.com>
2023-10-16 13:15:29 +0530

    Delete level1 calculator directory

--- level1 calculator/script.js
+++
@@ -1,8 +0,0 @@
-const inputElement = document.getElementById("screen");
-
-inputElement.addEventListener("keyup", function(event) {
-  if (event.key === "Enter") {
-    inputElement.value = eval(inputElement.value);
-  }
-});
-
